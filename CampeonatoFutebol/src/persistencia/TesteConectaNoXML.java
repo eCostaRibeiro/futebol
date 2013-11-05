fe654af4281a1682f2144f90793c01a959aa428d
Stwuart-STI <mustmusic.gc@live.com>
2013-11-05 21:51:53 -0200

    package persistencia

---
+++ CampeonatoFutebol/src/persistencia/TesteConectaNoXML.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Stwuart
+ */
+public class TesteConectaNoXML {
+
+  /**
+     * @param args the command line arguments
+     * @throws java.sql.SQLException
+     */
+    public static void main(String[] args) throws SQLException {
+        UsuarioOracle usuario;
+        
+        UsaXML xml = new UsaXML();
+        
+        
+        
+        //apenas na primeira vez
+//        UsuarioOracle iniciaUsuario = new UsuarioOracle("jdbc:oracle:thin:@192.168.0.127:1521/orcl", "futebol", "futebol");
+//        xml.gravaXML(iniciaUsuario);
+        
+        
+        usuario = xml.leituraXML();     
+        
+        
+        try(Connection dbConecta = new ConexaoOracle().getConnection(usuario)){
+            System.out.println("Oracle esta OK");
+        }catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            System.out.println("Nao foi possivel Conectar!! "+ e.getMessage());
+        }
+    }//fim main
+    
+}
